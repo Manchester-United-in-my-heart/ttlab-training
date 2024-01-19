fe216a7404a4d903751149193525b78bc9c1b452
BuiDuongKham <129467586+BuiDuongKham@users.noreply.github.com>
2024-01-19 16:04:21 +0700

    refactorized

--- assets/js/bai1_2.js
+++ assets/js/bai1_2.js
@@ -2,9 +2,9 @@ let labels = document.querySelectorAll('label');
 let inputs = document.querySelectorAll('input[type="radio"]');
 labels.forEach((label) => {
   if (document.getElementById(label.attributes['for'].value).checked) {
+    label.classList.add('text-blue-500');
     label.classList.add('bg-white');
     label.classList.add('rounded');
-    label.classList.add('text-blue-500');
   }
 });
 
@@ -12,8 +12,8 @@ inputs.forEach((input) => {
   input.addEventListener('change', (e) => {
     labels.forEach((label) => {
       if (document.getElementById(label.attributes['for'].value).checked) {
-        label.classList.add('bg-white');
         label.classList.add('text-blue-500');
+        label.classList.add('bg-white');
         label.classList.add('rounded');
       } else {
         label.classList.remove('bg-white');
