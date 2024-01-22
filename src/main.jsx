988644c14c0076ce6bf6c682b136c532dcfcc980
BuiDuongKham <129467586+BuiDuongKham@users.noreply.github.com>
2024-01-22 10:45:04 +0700

    login page

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Root from "./routes/root"
+import ErrorPage from './routes/error-page';
+import Contact from './routes/contact';
+import Login from './routes/login';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/contacts/:id',
+        element: <Contact />,
+      }
+    ]
+  },
+   {
+    path: '/contacts/:id',
+    element: <Contact />,
+   }, 
+   {
+    path: '/login',
+    element: <Login />,
+   }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
